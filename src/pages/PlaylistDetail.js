d518c83d469bc03b66c8cb37fe04bf24ead71612
Priyanshu <prynsu@yahoo.com>
2021-04-07 17:05:55 +0530

    Fix: Video player, playlist details and a button

--- src/pages/PlaylistDetail.js
+++ src/pages/PlaylistDetail.js
@@ -5,16 +5,16 @@ import { useApp } from "../contexts/AppContext"
 const PlaylistDetail = () => {
     const { app } = useApp()
     const { playlistId } = useParams();
-    const playlistToDisplay = app.playlists.find(playlist => playlist.id === playlistId)
+    const playlistToDisplay = app.playlists.find(playlist => playlist.id === parseInt(playlistId))
     return (
         <div className="videosDisplay">
             {playlistToDisplay.videos.map(video => {
                 return (
                     <Link className="link" to={`/video-player/${video.id}`}>
                         <VideoCard
-                        thumbnail={video.thumbnail}
-                        name={video.name}
-                        category={video.category}/>
+                            thumbnail={video.thumbnail}
+                            name={video.name}
+                            category={video.category} />
                     </Link>
                 )
             })}
--- src/pages/VideoPlayer.js
+++ src/pages/VideoPlayer.js
@@ -8,17 +8,17 @@ const VideoPlayer = () => {
     const [showPlaylists, setShowPlaylists] = useState(false);
     const [newPlaylist, setNewPlaylist] = useState("")
     const { videoId } = useParams()
-    const videoToPlay = app.videos.find(video => (video.id === videoId))
+    const videoToPlay = app.videos.find(video => (video.id === parseInt(videoId)))
     return (
         <div className="videoPlayer">
             <iframe className="videoArea" src={videoToPlay.link} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
             <h3>{videoToPlay.name}</h3>
-            <button className="btnBgNone" style={app.likedVideos.includes(videoToPlay) ? {color: "skyblue"} : {}} onClick={() => dispatch({ TYPE: "likeVideo", PAYLOAD: videoToPlay })}>{app.likedVideos.includes(videoToPlay) ? "Liked" : "Like"}</button>
+            <button className="btnBgNone" style={app.likedVideos.includes(videoToPlay) ? { color: "skyblue" } : {}} onClick={() => dispatch({ TYPE: "likeVideo", PAYLOAD: videoToPlay })}>{app.likedVideos.includes(videoToPlay) ? "Liked" : "Like"}</button>
             <button className="btnBgNone" onClick={() => setShowPlaylists(value => !value)}>Add to Playlist</button>
             <div style={showPlaylists ? { display: "" } : { display: "none" }}>
                 <div className="inputDiv">
-                    <input  className="input" value={newPlaylist} onChange={(e) => setNewPlaylist(e.target.value)} />
-                    <button className="btnIcon btnBgNone" onClick={() => {
+                    <input className="input" value={newPlaylist} onChange={(e) => setNewPlaylist(e.target.value)} />
+                    <button className="btnBgNone" onClick={() => {
                         if (newPlaylist === "") {
                             return ""
                         } else {
@@ -28,9 +28,9 @@ const VideoPlayer = () => {
                     }}>+</button>
                 </div>
                 {app.playlists.length === 0 ? "You don't have any playlist, create one." : app.playlists.map(list => <li className="listStyleNone cursorPointer" onClick={() => {
-                    if (list.videos.includes(videoToPlay)){
+                    if (list.videos.includes(videoToPlay)) {
                         return ""
-                    } else {dispatch({ TYPE: "addToPlaylist", PLAYLIST: list, PAYLOAD: videoToPlay })}
+                    } else { dispatch({ TYPE: "addToPlaylist", PLAYLIST: list, PAYLOAD: videoToPlay }) }
                 }}>{list.name}</li>)}
             </div>
         </div>
